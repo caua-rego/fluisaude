299795ee713a78a5f2395f664d7450999ac585d1
caua-rego <cauarego6@gmail.com>
2025-10-22 21:21:14 -0300

    chore: add .gitignore, update readme and main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 '''
 Main do projeto
 '''
+from flask import Flask
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
