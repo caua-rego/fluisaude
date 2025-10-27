7c8516221f9f8f01513c1ec68469285ea827a430
guialveess <97guilherme.alves@gmail.com>
2025-10-27 16:30:35 -0300

    feat(database): add Supabase PostgreSQL connection engine

---
+++ database.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
