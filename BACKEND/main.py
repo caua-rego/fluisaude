89b851122f3863a3f6eff4481f5cbf768c67222a
Cauã <cauarego6@gmail.com>
2025-11-03 11:43:30 -0300

    Delete BACKEND directory

--- BACKEND/main.py
+++
@@ -1,6 +0,0 @@
-from database.connection import Base, engine
-from app.models.consulta import Consulta
-
-if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
-    print("Tabela 'consultas' criada com sucesso!")
